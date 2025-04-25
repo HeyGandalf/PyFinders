4b5aa3b7039ba115c0c6d292f9d8e8d7c97ecf0b
Inna <Shelestova.ii@yandex.ru>
2025-04-25 19:54:19 +0700

    Переход на верный URL при авторизации добавила фикстуры

--- HW1/test_login_inna.py
+++ HW1/test_login_inna.py
@@ -1,13 +1,19 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import (By)
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+import pytest
 
-def test_error_user():
+@pytest.fixture()
+def driver():
     driver = webdriver.Chrome()
+    driver.maximize_window()
+    yield driver
+def test_error_user(driver):
     driver.get("https://www.saucedemo.com")
     driver.find_element(By.ID, value='user-name').send_keys("error_user")
     driver.find_element(By.ID, value='password').send_keys("secret_sauce")
     driver.find_element(By.NAME, value='login-button').click()
-
     assert driver.current_url == "https://www.saucedemo.com/inventory.html"
     driver.quit()
 
